a1a438f0238c32148ee243b16fb547d681015c20
zuper1998 <andras.mihaly@edu.bme.hu>
2021-10-11 22:25:34 +0200

    RETRO MID TO GO Also the MID_LOW needs to be done

--- src/Data/SimValues.java
+++ src/Data/SimValues.java
@@ -15,7 +15,7 @@ public class SimValues {
     public static final double entanglementGenHz = 52.36*1000; //https://www.nature.com/articles/s41534-021-00462-7
     public static final double frequencyBinEntanglementFidelity = 0.9574;
     public static final int SearchDepth = 5;
-    public static final String satData = "src/Data/QSAT_CROS_MID.txt";
+    public static final String satData = "src/Data/QSAT_RETRO_MID.txt";
     public static final String cityData = "src/Data/cities.txt";
     public static final int concurentThreads =6;
     public static ThreadLocal<QuantumBitTransmitanceCalculator> calc = new ThreadLocal<>();
