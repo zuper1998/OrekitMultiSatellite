8d87ade9c95abaad5d58331adb21befd5e6c2b77
zuper1998 <andras.mihaly@edu.bme.hu>
2021-10-12 13:45:57 +0200

    new satgens with 180 degree

--- src/Data/Output/satGen.py
+++ src/Data/Output/satGen.py
@@ -1,13 +1,13 @@
 
 def makeSats(SatNum: int):
-    f=  open("../QSAT_RETRO_HIGH.txt","w")
+    f=  open("../QSAT_CROS_LOWMID1.txt","w")
     i = 0
     raanIndex = 0
     maxAngle = 360 
-    angleDelta= 25
-    for raan in range(0,maxAngle,angleDelta):
+    angleDelta= 45
+    for raan in range(0,maxAngle,angleDelta): #2
         raanIndex+=1
-        for perige in range(0,maxAngle,angleDelta):
+        for perige in range(0,180,angleDelta): #1
             i+=1
             print(f"1000|0.0002090|{56.0568+180*(raanIndex%2)}|{perige}|{raan}|18.0| SAT_{i}",file = f)
             #print(f"satellites LOAD     1000     0.0002090     {56.0568+90*(raanIndex%2)}     {perige}     {raan}     18.0     1 1 1 1")
